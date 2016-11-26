57cae2ce235f20e7e08943fdea012d026a2daff7
Chivus <Chivus@Chivus-PC>
2016-11-25 23:12:46 -0600

    Coach with a new Java class for TrackCoach

--- src/com/luv2code/springdemo/MyApp.java
+++ src/com/luv2code/springdemo/MyApp.java
@@ -4,7 +4,7 @@ public class MyApp {
 
 	public static void main(String[] args) {
 		
-		Coach theCoach = new BaseballCoach();
+		Coach theCoach = new TrackCoach();
 		
 		System.out.println(theCoach.getDailyWorkout());
 
---
+++ src/com/luv2code/springdemo/TrackCoach.java
@@ -0,0 +1,11 @@
+package com.luv2code.springdemo;
+
+public class TrackCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		// TODO Auto-generated method stub
+		return "Run a Hard 5k";
+	}
+
+}
