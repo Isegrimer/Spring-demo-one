0a6519e74ebc6f90a02020d11b95c5732e6643da
Chivus <Chivus@Chivus-PC>
2016-11-26 12:16:04 -0600

    dependency Injection to a set method form XML file.

---
+++ src/com/luv2code/springdemo/CricketCoach.java
@@ -0,0 +1,31 @@
+package com.luv2code.springdemo;
+
+public class CricketCoach implements Coach {
+
+	private FortuneService fortuneService;
+	
+	//non arguments constructor
+	public CricketCoach() {
+		System.out.println("CricketCoach :  inside no-arg constructor");
+	}
+	
+	//our setter method
+	public void setFortuneService(FortuneService fortuneService) {
+		System.out.println("CricketCoach :  inside setter method - setFortuneService");
+		this.fortuneService = fortuneService;
+	}
+
+
+	@Override
+	public String getDailyWorkout() {
+		
+		return "Practice Fast Boeling for 15 minutes";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		
+		return fortuneService.getFortune();
+	}
+
+}
---
+++ src/com/luv2code/springdemo/SetterDemoApp.java
@@ -0,0 +1,25 @@
+package com.luv2code.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SetterDemoApp {
+
+	public static void main(String[] args) {
+		
+		//load the Spring configuration file
+		ClassPathXmlApplicationContext context = 
+				new ClassPathXmlApplicationContext("applicationContext.xml");
+		//retrieve bean from spring container
+		CricketCoach theCoach = context.getBean("myCricketCoach",CricketCoach.class);
+		
+		//call methods on the bean
+		
+		System.out.println(theCoach.getDailyWorkout());
+		System.out.println(theCoach.getDailyFortune());
+		
+		//close the context
+		context.close();
+
+	}
+
+}
