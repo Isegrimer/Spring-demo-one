8ea0aceffa9ff2c925e34dc7cb4f9c9122d61316
Chivus <Chivus@Chivus-PC>
2016-11-25 23:03:36 -0600

    Coach. fist practice

---
+++ src/com/luv2code/springdemo/MyApp.java
@@ -0,0 +1,13 @@
+package com.luv2code.springdemo;
+
+public class MyApp {
+
+	public static void main(String[] args) {
+		
+		Coach theCoach = new BaseballCoach();
+		
+		System.out.println(theCoach.getDailyWorkout());
+
+	}
+
+}
